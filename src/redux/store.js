18430ad0f60bf7be296cea680854f3cbf824c55c
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-09-04 19:28:22 +0200

    Add search function

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,12 +11,12 @@ const reducer = (state, action) => {
       return { ...state, columns: [...state.columns, {...action.payload, id: shortid() }]}
     case 'ADD_CARD' :
       return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }] };
+    case 'UPDATE_SEARCHSTRING' :
+      return { ...state, searchString: action.payload };
     default: 
       return state;
   }
-    //if(action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, {...action.payload, id: shortid() }]}
-    //return state;
-  };
+};
 
 //pierwszy argument - funkcja która odpowiada za modyfikację danych z magazynu
 //drugi argument - informacja o danych startowych magazynu
